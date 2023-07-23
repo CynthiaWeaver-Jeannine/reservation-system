907200962340ad5ea4186a82af299393c531c577
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-07-23 00:28:22 -0400

    add comments

--- front-end/src/tables/TableFinish.js
+++ front-end/src/tables/TableFinish.js
@@ -1,10 +1,21 @@
-/** @format */
+/** @format
+ * 
+ * Path: front-end\src\tables\TableFinish.js
+ */
 
 import React from "react";
 
 //import components
 import ErrorAlert from "../layout/ErrorAlert";
 
+/**
+ * Defines the table finish button.
+ * @param table
+ * the table to be finished
+ * @param clickHandler
+ * the function to call when the button is clicked
+ * @returns {JSX.Element}
+ */
 const TableFinish = ({ table, clickHandler, error, ...props }) => {
 	return (
 		table.reservation_id && (
@@ -12,7 +23,7 @@ const TableFinish = ({ table, clickHandler, error, ...props }) => {
 				<ErrorAlert error={error} />
 				<button
 					{...props}
-					className="btn btn-danger"
+					className="btn btn-warning"
 					type="button"
 					onClick={(e) => clickHandler(e, table.table_id)}>
 					Finish
